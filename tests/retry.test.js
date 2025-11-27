bdd11e400357c969876054f0ad902e0efd80c035
Jun <jun@your-email.com>
2025-11-27 09:15:26 +0000

    fix: update package name to magi-sys in tests

--- tests/retry.test.js
+++ tests/retry.test.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 try {
   const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
   // package.json に期待する name が入っているかの簡単なチェック
-  assert.strictEqual(pkg.name, 'magi-core', 'package.json name should be magi-core');
+  assert.strictEqual(pkg.name, 'magi-sys', 'package.json name should be magi-sys');
   console.log('retry.test.js: OK');
 } catch (err) {
   console.error('retry.test.js: FAILED', err);
